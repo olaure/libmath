2d8b9c1ad37b10f16d8a55de799abd02da28baf0
Oscar LAURENT <olaurent@student.42.fr>
2016-12-01 15:47:27 +0100

    Wrongful transposition of the cross matrix

--- src/ft_veccmat.c
+++ src/ft_veccmat.c
@@ -6,11 +6,12 @@
 /*   By: olaurent <olaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/12 12:20:00 by olaurent          #+#    #+#             */
-/*   Updated: 2016/11/30 19:14:49 by olaurent         ###   ########.fr       */
+/*   Updated: 2016/12/01 14:55:16 by olaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_math.h"
+#include "ft_matrices.h"
 
 /*
 ** This outputs the cross matrix associated with a vector
@@ -26,13 +27,13 @@ t_mat		*ft_veccmat(t_vec *v)
 	if (!(m = ft_newmat(v->d, v->d)))
 		return (NULL);
 	m->m[0][0] = 0;
-	m->m[0][1] = v->v[2];
-	m->m[0][2] = -v->v[1];
-	m->m[1][0] = -v->v[2];
+	m->m[0][1] = -v->v[2];
+	m->m[0][2] = v->v[1];
+	m->m[1][0] = v->v[2];
 	m->m[1][1] = 0;
-	m->m[1][2] = v->v[0];
-	m->m[2][0] = -v->v[2];
-	m->m[2][1] = v->v[1];
+	m->m[1][2] = -v->v[0];
+	m->m[2][0] = -v->v[1];
+	m->m[2][1] = v->v[0];
 	m->m[2][2] = 0;
 	return (m);
 }

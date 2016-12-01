8fed8a8c9764987cdb5e64b1d2491ef80fa24ffc
Oscar LAURENT <olaurent@student.42.fr>
2016-12-01 15:44:44 +0100

    better performance of matmul

--- src/ft_matmul.c
+++ src/ft_matmul.c
@@ -6,7 +6,7 @@
 /*   By: olaurent <olaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/11 11:03:00 by olaurent          #+#    #+#             */
-/*   Updated: 2016/09/13 12:50:19 by olaurent         ###   ########.fr       */
+/*   Updated: 2016/12/01 14:23:33 by olaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@ t_mat		*ft_matmul(t_mat *m, double c, t_mat *dest)
 
 	if (dest && !(dest->l == m->l && dest->c == m->c))
 		return (NULL);
-	if (!(mr = ft_newmat(m->l, m->c)))
+	if (dest)
+		mr = dest;
+	else if (!(mr = ft_newmat(m->l, m->c)))
 		return (NULL);
 	i = -1;
 	while (++i < m->l)
@@ -29,9 +31,5 @@ t_mat		*ft_matmul(t_mat *m, double c, t_mat *dest)
 		while (++j < m->c)
 			mr->m[i][j] = m->m[i][j] * c;
 	}
-	if (!dest)
-		return (mr);
-	ft_matcpy(mr, dest);
-	ft_delmat(&mr);
-	return (dest);
+	return (mr);
 }

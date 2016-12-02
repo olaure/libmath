3fdb0863aa52de29a32c4f24ecc4b9886cedae15
Oscar LAURENT <olaurent@student.42.fr>
2016-12-02 18:47:38 +0100

    tan function

--- src/ft_tan.c
+++ src/ft_tan.c
@@ -1,29 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_math.h                                          :+:      :+:    :+:   */
+/*   ft_tan.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: olaurent <olaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/09/10 09:29:17 by olaurent          #+#    #+#             */
-/*   Updated: 2016/12/02 17:44:36 by olaurent         ###   ########.fr       */
+/*   Created: 2016/12/02 17:43:31 by olaurent          #+#    #+#             */
+/*   Updated: 2016/12/02 17:44:21 by olaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_MATH_H
-# define FT_MATH_H
-# include <stdlib.h>
-# include "ft_consts.h"
-# include "ft_vectors.h"
-# include "ft_matrices.h"
+#include "ft_math.h"
 
-double				ft_sqrt(double n);
-TT					ft_sin(TT a);
-TT					ft_cos(TT a);
-TT					ft_tan(TT a);
-double				ft_exp(double x);
-double				ft_pow(double x, int n);
-double				ft_sigmoid(double x);
-double				ft_sigmoid_prime(double x);
-
-#endif
+TT		ft_tan(TT a)
+{
+	return (ft_cos(a) / ft_sin(a));
+}
